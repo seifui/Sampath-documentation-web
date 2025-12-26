485ddaec2a9aa5cc966bbbbb7f42a5fee64d1f3c
isharainduranga <ytube9065@gmail.com>
2025-12-26 09:43:23 +0530

    pattern img change commit

--- src/pages/PatternPage/ErrorPatternPage/ErrorPatternPage.page.tsx
+++ src/pages/PatternPage/ErrorPatternPage/ErrorPatternPage.page.tsx
@@ -34,7 +34,6 @@ export function ErrorPatternPage() {
                 <li>Allow users to dismiss non-blocking errors</li>
               </ul>
             </section>
-
         </div>
       </div>
     </div>
